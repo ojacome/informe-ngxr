ac9e4baa6fe8c0bacc6d652b71d224e1250c1018
olmedo <olmedo,bdp@gmail.com>
2021-02-09 13:06:27 -0500

    loading desde el store al form de ingreso egresos

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,29 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
 import { IngresoEgresoService } from '../services/ingreso-egreso.service';
 import { IngresoEgresoModel } from '../models/ingreso-egreso.model'
-import Swal from 'sweetalert2';
+import { isLoading, stopLoading } from '../shared/ui.actions';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
   styles: [
   ]
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
 
   ingresoForm: FormGroup;
   tipo : string = 'ingreso';
+  cargando: boolean;
+  isLoadingSub: Subscription;
 
 
 
   constructor(
     private fb: FormBuilder,
-    private ingresoEgresoSvc : IngresoEgresoService
+    private ingresoEgresoSvc : IngresoEgresoService,
+    private store: Store<AppState>
   ) { }
-
+  
   ngOnInit(): void {
-
+    
     this.crearFormulario();
+    this.cargarStore();
+  }
+  ngOnDestroy(): void {
+    this.isLoadingSub.unsubscribe();
+  }
+  
+
+
+  cargarStore(){
+    this.isLoadingSub = this.store.select('ui')
+    .subscribe( ({ isLoading }) => this.cargando = isLoading )
   }
 
   crearFormulario(){
@@ -36,11 +55,16 @@ export class IngresoEgresoComponent implements OnInit {
   guardar(){  
     const { description, monto } = this.ingresoForm.value;
     const ingresoEgreso = new IngresoEgresoModel( description, monto, this.tipo );
+    this.store.dispatch( isLoading() );
     this.ingresoEgresoSvc.crearIngresoegreso( ingresoEgreso )    
     .then( () => {
+      this.store.dispatch( stopLoading() );
       this.ingresoForm.reset();
       Swal.fire(' Registro Creado', description , 'success');
     } )
-    .catch( err => console.warn(err) )
+    .catch( err => {
+      this.store.dispatch( stopLoading() );      
+      console.warn(err)
+    } )
   }
 }
