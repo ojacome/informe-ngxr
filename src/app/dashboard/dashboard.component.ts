246dfb96580144a5ada8968cfcbae9dd927cccca
olmedo <olmedo,bdp@gmail.com>
2021-02-09 16:52:20 -0500

    guardar items en el storage

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { AppState } from '../app.reducer';
+import { setItems } from '../ingreso-egreso/ingreso-egreso.actions';
+import { IngresoEgresoService } from '../services/ingreso-egreso.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,11 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userSub :Subscription;
+  ingresoSub: Subscription;
 
+
+
+  constructor(
+    private store: Store<AppState>,
+    private ingresoEgresoSvc : IngresoEgresoService
+  ) { }
+  
   ngOnInit(): void {
+    this.cargarStore();
+  }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+    this.ingresoSub.unsubscribe();
   }
 
+
+
+  cargarStore() {
+    this.userSub = this.store.select('user')
+    .pipe(
+      filter( auth => auth.user != null )
+    )
+    .subscribe( ({user}) => {
+      this.ingresoSub = this.ingresoEgresoSvc.initIngresoEgresoListener( user.uid_ )
+      .subscribe( ingresosEgresos => {
+        
+        this.store.dispatch( setItems( { items: ingresosEgresos }))
+      })
+    })
+  }
+
+  
 }
